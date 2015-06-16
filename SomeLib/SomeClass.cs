5731478bda1304ae884c7c5da1ad9ec76926aabf
Masamoto Miyata <miyata@sincere-co.com>
2015-06-16 16:39:49 +0900

    inital commit

---
+++ SomeLib/SomeClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SomeLib
+{
+    public class SomeClass
+    {
+        public string getBar()
+        {
+            return "bar";
+        }
+    }
+}
---
+++ XUnitTest/SomeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+using SomeLib;
+
+namespace XUnitTest
+{
+    public class SomeTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            SomeClass foo = new SomeClass();
+
+            Assert.Equal("aaa", foo.getBar());
+        }
+    }
+}
