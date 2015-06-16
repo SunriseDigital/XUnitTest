ecabc58cedb3da41f69b4aeb607ab609a4492f31
Masamoto Miyata <miyata@sincere-co.com>
2015-06-16 18:17:07 +0900

    1.9.2に変更してみた。

---
+++ XUnitTest/SomeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+using SomeLib;
+
+namespace XUnitTest
+{
+    public class SomeTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            SomeClass foo = new SomeClass();
+            Assert.Equal("hoge", foo.getBar());
+        }
+    }
+}
